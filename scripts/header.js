a183deeeeefba47b83d9ba6f18b791b6fd875df2
LaiThiHoa01 <23130111@st.hcmuaf.edu.vn>
2025-10-26 21:54:13 +0700

    fix

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,11 @@
 // tải nội dung header cho trang index, login, signup
-fetch("../components/header.html")
+fetch("components/header.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("header-placeholder").innerHTML = data;
     });
-// tải nội dung footer cho trang index
-fetch("../components/footer.html")
+// tải nội dung footer cho trang index, login, signup
+fetch("components/footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer-placeholder").innerHTML = data;
